d285b45033c736d777af28701554dbbe6e8ac57a
Candela96 <pablo.gonzalezcnd@uanl.edu.mx>
2016-10-29 12:54:48 -0500

    Archivos de app y gradel

---
+++ app/src/main/java/com/proyecto/pablo/findingtacos/agregarComentario.java
@@ -0,0 +1,13 @@
+package com.proyecto.pablo.findingtacos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class agregarComentario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_comentario);
+    }
+}
